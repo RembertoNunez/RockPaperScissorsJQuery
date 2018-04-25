7e1b95e7f20874f119707b9250b2d9ab89d20199
Remberto Nunez <remnunez@csumb.edu>
2018-04-25 21:58:36 +0000

    first commit

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 	<head>
 		<title>Rock Paper Scissors</title>
 		<link rel="stylesheet" type="text/css" href="rps.css">
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
  		<script language="javascript" type="text/javascript" src="rps.js"></script>
 	</head>
